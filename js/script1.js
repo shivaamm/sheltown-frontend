441478ec37c2ccb3a913ff5df8757ddf42acbd3e
Shivam Singhal <shivam.singhal04888@gmail.com>
2020-05-30 13:10:17 +0530

    Clean

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){ 
-$('.js--section-features').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
- })
-});
