b9201d25ce47e68d47fee92b87e950ee69f16748
Anwesha Patra <36663339+AnweshaPatra@users.noreply.github.com>
2020-06-16 20:23:30 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -147,4 +147,21 @@ jQuery(document).ready(function($) {
     }
   });
 
+  // Offers carousel (uses the Owl Carousel library)
+  $(".offers-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      900: {
+        items: 4
+      }
+    }
+  });
 });
